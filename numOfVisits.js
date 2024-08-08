ec1899b621840ec452d221c099c24588fb2e5dcc
JA-WRI <jadenwrightmaurais@gmail.com>
2024-08-08 19:55:36 -0400

    good

--- numOfVisits.js
+++ numOfVisits.js
@@ -10,13 +10,19 @@ const app = express();
 
 app.use(cookieParser());
 
+function formatESTDate() {
+    const now = new Date();
+    const estDate = new Date(now.toLocaleString("en-US", { timeZone: "America/New_York" }));
+    const dateString = estDate.toDateString();
+    const timeString = estDate.toTimeString().split(" ")[0];
+    return `${dateString.split(" ").slice(0, 3).join(" ")} ${timeString} EST ${estDate.getFullYear()}`;
+}
+
 app.get('/', (req, res) => {
     if (req.cookies.visits) {
-        const estDate = Date.now().toLocaleDate("en-US", { timeZone: "America/New_York" });
-        const est = new Date(estDate);
         let visits = parseInt(req.cookies.visits);
         res.cookie('visits', visits + 1);
-        res.send(`Hello, this is the ${visits + 1} time that you are visiting my webpage. Last time you visited my webpage on: ${estDate}`);
+        res.send(`Hello, this is the ${visits + 1} time that you are visiting my webpage. Last time you visited my webpage on: ${formatESTDate()}`);
     } else {
         res.cookie('visits', 1 );
         res.send("Welcome to my webpage! It is your first time that you are here.");
